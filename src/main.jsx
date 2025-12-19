fe5e0d79da960eb5ade59b8097868834ed31bac7
ethiotemesgen <temesgenbeleteshiferaw.a1@gmail.com>
2025-12-19 10:42:48 +0530

    cart update

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { DataProvider } from "./COMPONENT/DataProvider/DataProvider";
-import { reducer, initialState } from "./Utility/Reducer";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <DataProvider reducer={reducer} initialState={initialState}>
-      <BrowserRouter>
+    <DataProvider>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </DataProvider>
   </React.StrictMode>
 );
