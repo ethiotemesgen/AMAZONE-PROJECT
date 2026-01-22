391eb2ac6413d2520cdb9a0ac997cea8ec15e7e7
ethiotemesgen <temesgenbeleteshiferaw.a1@gmail.com>
2026-01-22 13:49:13 +0530

    updated

--- src/Utility/Firebase.jsx
+++ src/Utility/Firebase.jsx
@@ -3,7 +3,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-/
+
 
 const firebaseConfig = {
   apiKey: "AIzaSyBxRc6Ur9McihwsloamH0caKYN1Wm_gRhI",
