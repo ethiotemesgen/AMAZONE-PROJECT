0707a374af8b01983cf3658d82b3b565f3cdce6c
ethiotemesgen <temesgenbeleteshiferaw.a1@gmail.com>
2026-01-22 16:25:58 +0530

    a

--- src/Pages/Cart/Cart.jsx
+++ src/Pages/Cart/Cart.jsx
@@ -4,7 +4,7 @@ import Layout from "../../COMPONENT/Layout/Layout";
 import { DataContext } from "../../COMPONENT/DataProvider/DataProvider";
 import { Type } from "../../Utility/Action.type";
 import ProductCard from "../../COMPONENT/Product/ProductCard";
-import styles from "./Cart.module.css";
+import styles from "./Cart.module.css"; //cart module css
 
 const Cart = () => {
   const [{ basket }, dispatch] = useContext(DataContext);
